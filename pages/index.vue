bfee6da753daf0aa5646bceb9528f629e6482abb
bbj14 <kazuya1425m@gmail.com>
2020-07-27 19:37:43 +0900

    Lesson6

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
 export default {
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {
+        error({ statusCode: 503, message: 'Unable to fetch events.' })
+      })
+  },
   head() {
     return {
       title: 'event listing',
